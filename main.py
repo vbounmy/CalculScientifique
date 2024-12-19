5ebd108af723e81aed6eecfbd13af60bd1e7bdd8
vbounmy <valentine.bounmy@gmail.com>
2024-12-19 13:47:56 +0100

    Fixing algorithm : adding function solve_equation

--- main.py
+++ main.py
@@ -3,28 +3,25 @@ import numpy
 def f(x):
     return x**2 - 8 * numpy.log(x) #x**2 = x puissance 2
 
-x = numpy.array([1, 2, 3])
-y = f(x)
-#y = [f(1), f(2), f(3)] >> ce qui va être affiché 
+def solve_equation(f, left, right, precision=10**(-3)):
+    while right - left >= precision:
+        middle = (right + left) / 2
 
-print(y)
+        if f(middle) == 0:
+            print(middle)
+            break
 
-left = 1
-right = 2
-precision = 10**(-3)
+        elif f(left) * f(middle) < 0: #ça veut dire que left et middle ont des signes diff
+            right = middle
 
-while right - left >= precision:
-    middle = (right + left) / 2
+        elif f(right) * f(middle) < 0: #ça veut dire que right et middle ont des signes diff
+            left = middle
 
-    if f(middle) == 0:
-        print(middle)
-        break
+    return middle
 
-    elif f(left) * f(middle) < 0: #ça veut dire que left et middle ont des signes diff
-        right = middle
-
-    elif f(right) * f(middle) < 0: #ça veut dire que right et middle ont des signes diff
-        left = middle
-
-print(middle)
-print(f(middle))
+if __name__ == "__main__":
+    x = numpy.array([1, 2, 3])
+    y = f(x)
+    middle = solve_equation(f, 1, 2)
+    print(middle)
+    print(f(middle))
